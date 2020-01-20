375fd5a13133280433890f7a98fc8f1237db7e11
EC2 Default User <ec2-user@ip-172-31-47-190.ap-northeast-1.compute.internal>
2020-01-20 09:16:35 +0000

    Add getConnectionId function

--- src/main/java/com/github/fumkato/OnConnectHandler.java
+++ src/main/java/com/github/fumkato/OnConnectHandler.java
@@ -1,7 +1,30 @@
 package com.github.fumkato
 
 public class OnConnectHandler{
-  public Response handleRequest() {
+  public Response handleRequest(Map<String, Object> input, Context context) {
     
+  }
+  
+  private String getConnectionId(Map<String, Object> input) {
+    String endpointStr = System.getenv(WEBSOCKET_ENDPOINT_ENV);
+    if (endpointStr == null) {
+      return null;
+    }
+
+    try {
+      URI endpoint = new URI(endpointStr);
+      apiGwClient = ApiGatewayManagementApiClient.builder().endpointOverride(endpoint).build();
+    } catch (Exception e) {
+      System.err.println("Failed to construct a client of API Gateway.");
+      e.printStackTrace();
+      return null;
+    }
+
+    Object requestContext = input.get("requestContext");
+    if (!(requestContext instanceof Map)) {
+      return null;
+    }
+
+    return ((Map<String, String>) requestContext).get("connectionId");
   }
 }
